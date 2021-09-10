71491396d3731dfcb81e84848753752795574b05
aman-janwani <amanjanwani1486@gmail.com>
2021-09-10 09:37:40 +0530

    pwa

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 const withPWA = require("next-pwa");
+
 module.exports = withPWA({
   pwa: {
     dest: "public",
-    swSrc: "service-worker.js",
+    register: true,
+    skipWaiting: true,
   },
+
   images: {
     domains: [
       "links.papareact.com",
@@ -12,9 +15,10 @@ module.exports = withPWA({
       "lh3.googleusercontent.com",
     ],
   },
-  env: {
+env: {
     mapbox_key:
       "pk.eyJ1IjoiYW1hbi1qYW53YW5pIiwiYSI6ImNrczBpM2gxbTFrcm8ydXFqamtybzJtYW8ifQ.EcMfJu_XkFN2RMfGUvX8zw",
     stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
   },
 });
+
